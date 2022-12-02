ba7640799dd09c5d08a69687165ae3687140ded4
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-02 15:11:29 +0100

    Fixed helper method for opening the file + usage

--- advent.py
+++ advent.py
@@ -1,10 +1,10 @@
-def get_file_name(exercise, ext="txt"):
-    return f"input_{exercise}.{ext}"
+def get_file(exercise, ext="txt"):
+    return open(f"input_{exercise}.{ext}", "r", encoding="utf-8")
 
 
 
-def day_1_main(name):
-    with open("input_day_1_main.txt", "r", encoding="utf-8") as f:
+def day_1(name):
+    with get_file(name) as f:
         # sepparate the groups and numbers by a different character, to make it easier
         raw_numbers = f.read().replace("\n\n",";").replace("\n",",")[:-1]
         # split the groups and sum the numbs
@@ -13,6 +13,6 @@ def day_1_main(name):
         return max(elfs_totals)
 
 def main():
-    print(day_1_main(get_file_name("day_1_main")))
+    print(day_1("day_1_main"))
 
 main()
