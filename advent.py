4039d44ed2f4b363902fc371287db6aa6f007334
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-02 16:26:27 +0100

    Day 2 - Main exercise

--- advent.py
+++ advent.py
@@ -1,7 +1,71 @@
+import enum 
 def get_file(exercise, ext="txt"):
     return open(f"input_{exercise}.{ext}", "r", encoding="utf-8")
 
 
+def day_2(name):
+    class Move_them(enum.StrEnum):
+        ROCK = "A"
+        PAPER = "B"
+        SCISSORS = "C"
+    class Move_me(enum.StrEnum):
+        ROCK = "X"
+        PAPER = "Y"
+        SCISSORS = "Z"
+
+    class Shape(enum.IntEnum):
+        # Points for the shape
+        ROCK = 1 
+        PAPER = 2
+        SCISSORS = 3
+    class Outcome(enum.IntEnum):
+        # Points for the outcome
+        LOSS = 0
+        DRAW = 3
+        WIN = 6 
+    
+    def get_move(move, move_map : Move_them | Move_me) -> Shape:
+        
+        match move:
+            case move_map.ROCK:
+                return Shape.ROCK
+            case move_map.PAPER:
+                return Shape.PAPER
+            case move_map.SCISSORS:
+                return Shape.SCISSORS
+            case _:
+                raise ValueError("Invalid argument", move, move_map)
+
+    def get_outcome_as_win(them : Shape, me : Shape) -> Outcome:
+        
+        if them == me:
+            return Outcome.DRAW
+        if them is Shape.ROCK:
+            if me is Shape.PAPER:
+                return Outcome.WIN
+            return Outcome.LOSS
+        
+        if them is Shape.PAPER:
+            if me is Shape.SCISSORS:
+                return Outcome.WIN
+            return Outcome.LOSS
+
+        if them is Shape.SCISSORS:
+            if me is Shape.ROCK:
+                return Outcome.WIN
+            return Outcome.LOSS
+
+    def get_score(them, me) -> int:
+
+        theirs, mine = get_move(them, Move_them),get_move(me,Move_me)
+        
+        return mine + get_outcome_as_win(theirs, mine)
+
+
+    results = [] 
+    with get_file(name) as f:
+        results = [get_score(*line.strip().split(" ")) for line in f]
+        return sum(results)
 
 def day_1(name):
     with get_file(name) as f:
@@ -17,7 +81,7 @@ def day_1(name):
 def main():
     fun_prefix = "day_"
     fun_names = [f"{fun_prefix}{i}" for i in range(1, 26)]
-    STOP = 2
+    STOP = 3
     STOP_AT = f"{fun_prefix}{STOP}" 
 
     clean_globals = {k:v for k,v in globals().items() if k.startswith(fun_prefix)}
