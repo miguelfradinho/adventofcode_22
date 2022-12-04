3ebd0ef241388471864ba58c5d338236da98740d
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-04 17:52:11 +0100

    Day 4 - Code cleanup

--- sol_snake/day_4.py
+++ sol_snake/day_4.py
@@ -4,11 +4,16 @@ from typing import Tuple
 def split_section(section):
     start, end = section.split("-")
     # need the extra +1 because range is exclusive
-    return int(start), int(end)+1
+    return int(start), int(end) + 1
+
+
 def pair_into_range_set(pair):
     first, second = pair
 
-    return set([i for i in range(*split_section(first))]), set([i for i in range(*split_section(second))])
+    return set([i for i in range(*split_section(first))]), set(
+        [i for i in range(*split_section(second))]
+    )
+
 
 def day_4(file_obj):
     with file_obj as f:
@@ -16,17 +21,15 @@ def day_4(file_obj):
 
     assignments = [pair_into_range_set(i) for i in all_pairs]
 
-    count = 0
-
     subsets = []
     overlap = []
     for a in assignments:
-        a : Tuple[set, set]
+        a: Tuple[set, set]
         f, s = a
 
         if f.intersection(s) != set() or s.intersection(f) != set():
-            overlap.append((f,s))
+            overlap.append((f, s))
         if s.issubset(f) or f.issubset(s):
-            subsets.append((f,s))
+            subsets.append((f, s))
 
-    return  len(subsets), len(overlap)
+    return len(subsets), len(overlap)
