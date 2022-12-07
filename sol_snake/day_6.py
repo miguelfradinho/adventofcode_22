a0186275af501fb25d54e166f963657b11710de6
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-07 15:23:45 +0100

    Day 6 - Added solution for first challenge

--- sol_snake/day_6.py
+++ sol_snake/day_6.py
@@ -1,2 +1,14 @@
 def day_6(file_obj):
-    return None
+    data_stream = None
+    with file_obj as f:
+        data_stream = f.read().strip()
+    stream_size = len(data_stream)
+    MARKER_SIZE = 4
+
+    for i in range(stream_size-MARKER_SIZE):
+        last_4: str = data_stream[i:i+MARKER_SIZE]
+        unique_chars = set(last_4)
+        # we found 4 unique
+        if len(unique_chars) == MARKER_SIZE:
+            return i+MARKER_SIZE
+    return -1
