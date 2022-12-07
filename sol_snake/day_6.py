0b51fba48a5fec848a0e2440c572ee11987061a9
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-07 15:28:48 +0100

    Day 6 - refactored code and added solution for 2nd challenge

--- sol_snake/day_6.py
+++ sol_snake/day_6.py
@@ -1,14 +1,18 @@
+def find_marker_after(stream : str, n_chars : int):
+    stream_size = len(stream)
+    for i in range(stream_size-n_chars):
+        last_n: str = stream[i:i + n_chars]
+        unique_chars = set(last_n)
+        # we found n unique
+        if len(unique_chars) == n_chars:
+            return i + n_chars
+
 def day_6(file_obj):
     data_stream = None
     with file_obj as f:
         data_stream = f.read().strip()
     stream_size = len(data_stream)
     MARKER_SIZE = 4
+    MESSAGE_SIZE = 14
 
-    for i in range(stream_size-MARKER_SIZE):
-        last_4: str = data_stream[i:i+MARKER_SIZE]
-        unique_chars = set(last_4)
-        # we found 4 unique
-        if len(unique_chars) == MARKER_SIZE:
-            return i+MARKER_SIZE
-    return -1
+    return find_marker_after(data_stream, MARKER_SIZE),  find_marker_after(data_stream, MESSAGE_SIZE)
