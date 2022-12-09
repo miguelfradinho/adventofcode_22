15273003366ee3b1d31f80d67db581af9ca647af
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-09 21:07:01 +0100

    Day 7 - Main challenge

--- sol_snake/day_7.py
+++ sol_snake/day_7.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass
-from typing import Optional
-
+from typing import Optional, IO
+from collections import deque
 
 @dataclass
 class File:
     name : str
     size : int
 
+
 class Dir:
-    def __init__(self, name: str, parent = None):
+    def __init__(self, name: str, parent : "Dir" = None):
         # name of this node
         self._name: str = name
         if parent is not None:
@@ -58,6 +59,111 @@ class Dir:
         self._total_size = curr_size
         return curr_size
 
-def day_7(file_obj):
+    def add_file(self, file: File):
+        self._files.append(file)
 
+    def add_subdirectory(self, sub_dir):
+        self._dirs.append(sub_dir)
 
+    def find_child(self, name: str) -> Optional["Dir"]:
+        for i in self._dirs:
+            if i.name == name:
+                return i
+        return None
+
+    def __repr__(self):
+        return f"({self.name}, dirs={str(self._dirs)}, files={str(self._files)})"
+
+def create_tree(file_obj):
+    # create the root node
+    ROOT_NODE = Dir("/")
+    all_dirs = [ROOT_NODE]
+    curr_node: Dir = None
+    # we could read directly off the file object
+    # however, since this implementation doesn't parse the input the first time
+    # we'll need some sort of buffer to handle this
+    # (or we could also do low level magic with file seek offsets... but high level is more fun)
+    # So, we use lists, which is the simplest way to use as a buffer
+    raw_lines = deque(i.strip("\n") for i in file_obj.readlines())
+    file_lines = deque(raw_lines)
+
+    has_next = True
+
+    while has_next:
+        try:
+            # read next line
+            line = file_lines.popleft()
+        except IndexError:
+            has_next = False
+            continue
+        # we reached end of file
+        if line == "":
+            break
+
+        # check which command
+        match line.lstrip("$ ").split(" "):
+            # change command
+            case ["cd", dir_name]:
+                match dir_name:
+                    # move one up
+                    case "..":
+                        curr_node = curr_node.parent
+                        continue
+                    # switch to the root
+                    case "/":
+                        curr_node = ROOT_NODE
+                        continue
+                    # switch to a subdirectory
+                    case dir_name:
+                        sub_dir = curr_node.find_child(dir_name)
+                        if sub_dir is None:
+                            raise ValueError(f"Error parsing, directory {dir_name} was not created")
+                        curr_node = sub_dir
+                        continue
+            # listing
+            case ["ls"]:
+                # it's a list, so we need to read until the next command
+                listed_files = []
+
+                while True:
+                    try:
+                        next_line = file_lines.popleft()
+                    # EOF
+                    except IndexError:
+                        break
+
+                    # next line is a command, so return it to the buffer
+                    if next_line.startswith("$"):
+                        file_lines.appendleft(next_line)
+                        break
+                    # it doesn't start with a command, so it's one of the listed files
+                    listed_files.append(next_line)
+
+                # okay, we read all the listed_files so, create it appropriately
+                for i in listed_files:
+                    dir_or_size, name = i.split(" ")
+                    dir_or_size: str
+                    # it's a directory, so create it
+                    if dir_or_size.startswith("dir"):
+                        new_node = Dir(name, curr_node)
+                        curr_node.add_subdirectory(new_node)
+                        all_dirs.append(new_node)
+                    # it's a file
+                    else:
+                        new_node = File(name, int(dir_or_size))
+                        curr_node.add_file(new_node)
+
+    return ROOT_NODE, all_dirs
+
+def day_7(file_obj: IO):
+    #example = open("sol_snake\example_7.txt")
+    # read the lines while also removing line breaks
+    tree, all_dirs = create_tree(file_obj)
+    MAX_SIZE = 100000
+    valid_dirs = []
+    for d in all_dirs:
+        dir_size = d.get_size
+        if dir_size <= MAX_SIZE:
+            valid_dirs.append(d)
+
+    return sum([i.get_size for i in valid_dirs])
