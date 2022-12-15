f796fa4dcb9f579c1d97f8ca3aaff39ea183eef8
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2022-12-15 15:27:36 +0100

    Day 13 - Initial logic for comparing, not fully 100% correct

--- sol_snake/day_13.py
+++ sol_snake/day_13.py
@@ -15,6 +15,7 @@ class Pair:
 def parse_input_packet(line):
     # extremely lazy mode (and insecure as well)
     return eval(line)
+
 def parse_input(file_obj : IO):
     # Each pair is separated by a blank line
     # We could read all lines and do replacement, but input file seems large enough to justify using
@@ -40,7 +41,60 @@ def parse_input(file_obj : IO):
                 pair.append(packet)
     return total_pairs
 
+def compare_values(left: PacketData, right : PacketData):
+
+    # if they both have the same type
+    if type(left) == type(right):
+        # both are integers
+        if type(left) == int:
+            # If the left integer is higher than the right integer, the inputs are not in the right order.
+            if left > right:
+                return False
+            # Otherwise, the inputs are the same integer; continue checking the next part of the input.
+            return True
+        # both are lists
+        elif type(left) == list:
+            # If the left list runs out of items first, the inputs are in the right order.
+            if len(left) < len(right):
+                return True
+            # If the right list runs out of items first, the inputs are not in the right order.
+            elif len(left) > len(right):
+                return False
+            # Keep comparing the rest, so
+            else:
+                comparisons = []
+
+                for i in range(len(left)):
+                    curr_left = left[i]
+                    curr_right = right[i]
+                    curr_res = compare_values(curr_left, curr_right)
+                    comparisons.append(curr_res)
+                return all(comparisons)
+
+    # If exactly one value is an integer, convert the integer to a list which contains that integer as its only value
+    if type(left) == int:
+        return compare_values([left], right)
+    elif type(right) == int:
+        return compare_values(left, [right])
+    else:
+        raise ValueError("we should never reach here", left, right)
+
+
 def day_13(file_obj):
     example = open("sol_snake\example_13.txt", encoding="utf-8")
     packet_pairs = parse_input(example)
-    return packet_pairs
+
+    correct_pairs = []
+
+    # for each pair
+    for i in range(len(packet_pairs)):
+        curr_pair = packet_pairs[i]
+        left = curr_pair.left
+        right = curr_pair.right
+
+        pair_is_correct = compare_values(left, right)
+        if pair_is_correct:
+            correct_pairs.append(i)
+
+    print(correct_pairs)
+    return sum(correct_pairs)
